c32270793a9048479e95389d0fe042a82b628701
Unknown <kirstenmanley7@gmail.com>
2017-11-28 20:22:01 -0700

    updated chicken AI
    chickenAI now includes jumping

--- Chicken Game/Assets/Scripts/ChickenAI.cs
+++ Chicken Game/Assets/Scripts/ChickenAI.cs
@@ -8,15 +8,25 @@ public class ChickenAI : MonoBehaviour {
   public Transform target; 
   public Transform chickenPen; 
   public int points = 10; 
-   
+  public int badPoints = 5;
+  public float jumpHeight;
+  private float jumpCountdown;
+
+  void Start(){
+    jumpCountdown = Random.Range(1f,7f);
+  } 
   void OnTriggerStay (Collider other) 
   { 
- 
-    if(other.gameObject.name == "Player"){ 
+    if(other.gameObject.name == "Wolf"){ 
+      Debug.Log("Wolf has entered chickens trigger"); 
+      transform.LookAt(target); 
+      transform.Translate(Vector3.back*moveSpeed*Time.deltaTime);
+    }
+    else if(other.gameObject.name == "Player"){ 
       Debug.Log("Player has entered chickens trigger"); 
       transform.LookAt(target); 
       transform.Translate(Vector3.back*moveSpeed*Time.deltaTime);
-    } 
+    }  
   } 
   void OnCollisionEnter(Collision other) 
   { 
@@ -25,7 +35,24 @@ public class ChickenAI : MonoBehaviour {
     transform.position = chickenPen.position; 
     transform.rotation = chickenPen.rotation; 
       }
-    } 
+    if(other.gameObject.name == "Wolf"){
+      scoreManager.SubtractPoints(badPoints);
+      Destroy(gameObject);
+      }
+    }
+  void Update(){
+    jumpCountdown -= Time.deltaTime;
+    if(jumpCountdown <= 0){
+        jumpCountdown = Random.Range(1f,7f);
+        Jump();
+    }
+  }
+  void Jump(){
+    RigidBody jumper = GetComponent<RigidBody>();
+    jumper.velocity = new Vector3(0,jumpHeight,0);
+    
+  }
+  
 } 
 
 //make it spicy

fead3755b5c73773957d8da9184fac53b3963520
Unknown <kirstenmanley7@gmail.com>
2017-12-04 20:17:39 -0700

    Added Domestication, Main Menu

---
+++ Chicken Game/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour {
+
+	public int levelToLoad;
+
+	public void LoadLevel(){
+		SceneManager.LoadScene(levelToLoad);
+	}
+	public void LevelExit(){
+		Application.Quit();
+	}
+
+
+}
